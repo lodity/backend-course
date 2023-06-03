673ca062d22595c1f48b3cba29c08d328af2bef2
lodity <lodity9@gmail.com>
2023-06-03 15:38:52 +0300

    added DB, Schema, Router

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log("SERVER WORKING");
+import express, { raw } from 'express';
+import mongoose from 'mongoose';
+import router from './router.js';
+
+const PORT = 5000;
+const DB_URL =
+	'mongodb+srv://user:user@cluster0.vj9ggz9.mongodb.net/?retryWrites=true&w=majority';
+
+const app = express();
+app.use(express.json());
+app.use('/api', router);
+
+async function startApp() {
+	try {
+		await mongoose.connect(DB_URL, {
+			useUnifiedTopology: true,
+			useNewUrlParser: true,
+		});
+		app.listen(PORT, () => console.log(`SERVER STARTED ON PORT ${PORT}`));
+	} catch (e) {
+		console.log(e);
+	}
+}
+startApp();
